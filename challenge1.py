1a8dfe5536bec8b31b780ac7bdace522006fc0a4
Coswold <coswoldo@gmail.com>
2019-07-14 14:38:22 -0700

    Challenge 1 update

--- challenge1.py
+++ challenge1.py
@@ -1,1 +1,13 @@
+import sys
 from graph_adt import Graph
+
+def buildGraph(txt):
+    f = open(txt, "w+")
+    for line in f:
+        print(line)
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        buildGraph(sys.argv[1])
+
