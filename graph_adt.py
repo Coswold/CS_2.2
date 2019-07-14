e530846071b0bd8da034889d00d7f7c53ec03bf1
Coswold <coswoldo@gmail.com>
2019-07-14 14:06:19 -0700

    Fixed broken add function

--- graph_adt.py
+++ graph_adt.py
@@ -25,13 +25,13 @@ class Vertex(object):
 
     def __str__(self):
         """output the list of neighbors of this vertex"""
-        return str(self.id) + " adjancent to " + str([x.id for x in self.neighbors])
+        return str(self.id) + " adjacent to " + str([x.id for x in self.neighbors])
 
     def getNeighbors(self):
         """return the neighbors of this vertex"""
         n = []
-        for key, value in self.neighbors.items():
-            n.append(key)
+        for neighbor in self.neighbors:
+            n.append(neighbor)
         return n
 
     def getId(self):
@@ -62,10 +62,11 @@ class Graph:
         """add a new vertex object to the graph with
         the given key and return the vertex
         """
-        self.numVertices += 1
         vert = Vertex(key)
-        self.vertList[key] = vert
-        return vert
+        if key not in self.vertList:
+            self.numVertices += 1
+            self.vertList[key] = vert
+        return self.vertList[key]
 
     def getVertex(self, n):
         """return the vertex if it exists"""
@@ -81,7 +82,7 @@ class Graph:
             self.addVertex(f)
         if t not in self.vertList:
             self.addVertex(t)
-        self.vertList[f].addNeighbor(t, cost)
+        self.vertList[f].addNeighbor(self.vertList[t], cost)
 
     def getCost(self, f, t):
         """return the cost of going from f to t"""
@@ -114,18 +115,37 @@ if __name__ == "__main__":
     g.addVertex("Friend 1")
     g.addVertex("Friend 2")
     g.addVertex("Friend 3")
+    g.addVertex("Friend 4")
+    g.addVertex("Friend 5")
+    g.addVertex("Friend 6")
+    g.addVertex("Friend 7")
+    g.addVertex("Friend 8")
+    g.addVertex("Friend 9")
+    g.addVertex("Friend 10")
+
 
     # ...  add all 10 including you ...
 
     # Add connections (non weighted edges for now)
     g.addEdge("Friend 1", "Friend 2")
     g.addEdge("Friend 2", "Friend 3")
+    g.addEdge("Friend 3", "Friend 4")
+    g.addEdge("Friend 4", "Friend 5")
+    g.addEdge("Friend 5", "Friend 6")
+    g.addEdge("Friend 6", "Friend 7")
+    g.addEdge("Friend 7", "Friend 8")
+    g.addEdge("Friend 8", "Friend 9")
+    g.addEdge("Friend 9", "Friend 10")
 
     # Challenge 1: Output the vertices & edges
     # Print vertices
-    print("The vertices are: ", g.getVertices(), "\n")
+    print("The vertices are: ", "\n")
+    for vert in g.getVertices():
+        print(vert)
+    print("\n")
 
-    print("The edges are: ")
+    print("The edges are: ", "\n")
     for v in g:
         for w in v.getNeighbors():
             print("( %s , %s )" % (v.getId(), w.getId()))
+
