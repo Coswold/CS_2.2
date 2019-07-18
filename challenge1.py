5087169af11d236460abbd5a4a9999bcd55b6c4c
Coswold <coswoldo@gmail.com>
2019-07-17 22:18:28 -0700

    Added verteces from file

--- challenge1.py
+++ challenge1.py
@@ -1,12 +1,24 @@
 import sys
 from graph_adt import Graph
 
-def buildGraph(txt):
+def read(txt):
     f = open(txt, "r")
-    for line in f:
-        print(line)
+    txt = f.read().split('\n')
+    txt.pop()
+    return txt
+
+def buildGraph(txt):
+    g = Graph()
+    verts = txt[1]
+    verts = verts.split(',')
+    for vert in verts:
+        g.addVertex(vert)
+
+    return g
 
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        buildGraph(sys.argv[1])
+        txt = read(sys.argv[1])
+        print(txt)
+        g = buildGraph(txt)
